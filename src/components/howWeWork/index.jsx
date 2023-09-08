8961a5227e55025b2ea591bf51355eae86da3e59
kiwi-ui <yusnianggara@gmail.com>
2023-09-08 07:04:57 +0700

    fix: fix how we work section layout

--- src/components/howWeWork/index.jsx
+++ src/components/howWeWork/index.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const HowWeWork = () => {
   return (
     <section className="mt-[30px] mb-[37px] h-max px-[60px] py-10 bg-gradientImg bg-no-repeat bg-cover">
-            <div className="flex flex-col">
+            <div className="flex flex-col justify-center">
                 <p className="text-center text-[32px] font-semibold text-white mb-16">How We Works</p>
-                <div className="p-[30px] flex flex-row bg-gradientImgBlack bg-cover bg-no-repeat gap-[50px] w-3/4 rounded-[10px]">
+                <div className="p-[30px] flex flex-row bg-gradientImgBlack bg-cover bg-no-repeat gap-[50px] w-3/4 rounded-[10px] mx-auto">
                     <img src="/assets/images/chat.png" alt="chat"/>
                     <div className="text-white">
                         <p className="text-xl font-semibold mb-[13px]">Estimate</p>
